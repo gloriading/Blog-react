c89d35ff2d5cefa35447bdc4731c41975798d162
Gloria Ding <gloria.hc.ding@gmail.com>
2018-02-03 15:34:02 -0800

    add CurrentDateTime

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import {PostShowPage} from './PostShowPage';
 import {PostIndexPage} from './PostIndexPage';
+import {CurrentDateTime} from './CurrentDateTime';
 
 function App(){
   return(
     <div className='App'>
+      <CurrentDateTime />
       <PostShowPage />
       <PostIndexPage />
     </div>
---
+++ src/components/CurrentDateTime.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+ class CurrentDateTime extends Component {
+   constructor (props) {
+     super(props);
+
+     this.state = {
+       dateTime: new Date()
+     }
+   }
+
+   componentDidMount () {
+     // code inside of this will run once the component
+     // is rendered in the DOM (i.e. visible)
+     this.intervalId = setInterval(
+       () => {
+         this.setState({dateTime: new Date()})
+       },
+       1000
+     );
+   }
+
+   componentWillUnmount () {
+     // code inside of this will run just before
+     // the component is removed from the DOM.
+     clearInterval(this.intervalId);
+     this.intervalId = null;
+   }
+
+
+
+   render () {
+     const {dateTime} = this.state;
+
+     return (
+       <span className="CurrentDateTime">
+         {dateTime.toLocaleDateString()} {dateTime.toLocaleTimeString()}
+       </span>
+     );
+   }
+ }
+
+ export {CurrentDateTime};
