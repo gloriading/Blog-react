358842b1936207e613cf3140fb10a2a2c7ec9ad1
Gloria Ding <gloria.hc.ding@gmail.com>
2018-02-03 13:59:50 -0800

    add state for PostShow and PostIndex

--- src/components/PostIndexPage.js
+++ src/components/PostIndexPage.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import posts from '../data/posts'
 
-function PostIndexPage(props){
-  return(
-    <main className="PostIndexPage" style={{paddingLeft: '20px'}}>
-      <h4 style={{fontSize:'30px', color:'green'}}>Post Index</h4>
-      <ul>
-        {
-          posts.map(post=>(
-            <li key={post.id} style={{fontSize:'20px'}}>
-              <a href="">{post.title}</a>
-            </li>
-          ))
-        }
-      </ul>
-    </main>
-  )
+class PostIndexPage extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      posts:posts
+    };
+  }
+
+  render(){
+    return(
+      <main className="PostIndexPage" style={{paddingLeft: '20px'}}>
+        <h4 style={{fontSize:'30px', color:'green'}}>Post Index</h4>
+        <ul>
+          {
+            this.state.posts.map(post=>(
+              <li key={post.id} style={{fontSize:'20px'}}>
+                <a href="">{post.title}</a>
+              </li>
+            ))
+          }
+        </ul>
+      </main>
+    )
+  }
 }
 
 export {PostIndexPage};
--- src/components/PostShowPage.js
+++ src/components/PostShowPage.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {PostDetails} from './PostDetails';
-import {CommentDetails} from './CommentDetails';
 import {CommentList} from './CommentList';
 import post from '../data/post'
 
 
-function PostShowPage(){
-  const {comments=[]} = post;
+class PostShowPage extends Component{
+  constructor (props) {
+    super(props);
+    this.state = {
+      post:post
+    };
+  }
 
-  const containerStyle = {
-    paddingLeft: '20px',
-    fontFamily: 'sans-serif',
-    fontSize: '20px'
-   }
-  return(
-    <main className="PostShowPage" style={containerStyle}>
-      <h4 style={{fontSize:'30px', color:'Red'}}>Post Show</h4>
-      <PostDetails {...post}/>
-      <CommentList comments={comments}/>
-    </main>
-  )
+  render(){
+
+    const {comments=[]} = this.state.post;
+
+    const containerStyle = {
+      paddingLeft: '20px',
+      fontFamily: 'sans-serif',
+      fontSize: '20px'
+     }
+    return(
+      <main className="PostShowPage" style={containerStyle}>
+        <h4 style={{fontSize:'30px', color:'Red'}}>Post Show</h4>
+        <PostDetails {...this.state.post}/>
+        <CommentList comments={comments}/>
+      </main>
+    )
+
+  }
 }
 
 export {PostShowPage};
